47ad200f3de985b67bfa9c775f9d93775f69f9c7
Zack Baker <zackabaker@gmail.com>
2015-07-15 17:13:20 -0400

    Initial Commit

---
+++ Votr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Votr
+//
+//  Created by Zachary Baker on 7/15/15.
+//  Copyright (c) 2015 zackabaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Votr/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Votr
+//
+//  Created by Zachary Baker on 7/15/15.
+//  Copyright (c) 2015 zackabaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
